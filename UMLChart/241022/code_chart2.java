31f2c568382f3b4c4c4d3245fcce4fb5de4bf666
JackChen5566 <147010437+JackChen5566@users.noreply.github.com>
2024-10-22 21:55:32 +0800

    update

---
+++ UMLChart/241022/code_chart2.java
@@ -0,0 +1,21 @@
+package java_class_241022;
+
+
+public interface A { 
+	   
+} 
+ 
+public class B implements A { 
+   
+} 
+
+
+
+public class code_chart2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ UMLChart/241022/code_chart3.java
@@ -0,0 +1,20 @@
+package java_class_241022;
+
+
+public class A { 
+    private B b; 
+    public A(B b){ 
+        this.b = b; 
+    }
+}
+
+
+
+public class code_chart3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
